c0ad70bed3e78cd0d613015a924ab272a3652e95
sapanafirke <133943002+sapanafirke@users.noreply.github.com>
2024-02-16 09:47:20 +0530

    minor bug fixed

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1213,7 +1213,7 @@ exports.getAllAdvances = catchAsync(async (req, res, next) => {
 
 exports.getAdvanceSummaryByEmployee = catchAsync(async (req, res, next) => {
   try {
-    const expenseAdvancelist = await Advancefind({}).where('employee').equals(req.params.userId);    
+    const expenseAdvancelist = await Advance.find({}).where('employee').equals(req.params.userId);    
    
     if (!expenseAdvancelist) {
       return res.status(404).json({
