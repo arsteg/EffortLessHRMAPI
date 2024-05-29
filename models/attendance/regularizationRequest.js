e04fb37238825806a8ae6b866aa9ca7d3d9f2e13
sapanafirke <133943002+sapanafirke@users.noreply.github.com>
2024-05-29 10:09:47 +0530

    added user in regularizationRequests model

--- models/attendance/regularizationRequest.js
+++ models/attendance/regularizationRequest.js
@@ -6,6 +6,11 @@ var regularizationRequestSchema = new Schema({
     type: Date,
     required: true
   },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
   requestType: {
     type: String,
     required: true
