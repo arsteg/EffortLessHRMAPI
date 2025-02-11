ca9467ab1abe79b942649bf6ba428c9298038d58
ARSTEG SOLUTIONS <info@arsteg.com>
2025-02-11 16:13:18 +0530

    PAssed the labels of the components

--- models/Payroll/ctcTemplateEmployerContributionModel.js
+++ models/Payroll/ctcTemplateEmployerContributionModel.js
@@ -18,5 +18,15 @@ const ctctemplateEmployerContributionSchema = new Schema({
     trim: true
   }
 }, { collection: 'CTCTemplateEmployerContribution' });
-
+ctctemplateEmployerContributionSchema.pre(/^find/,async function(next) {
+  try {
+    this.populate({
+      path: 'fixedContribution',
+      select: 'id label'
+    });
+  } catch (error) {
+    console.error("Error populating variable deductions:", error);
+  }
+  next();
+});
 module.exports = mongoose.model('CTCTemplateEmployerContribution', ctctemplateEmployerContributionSchema);
