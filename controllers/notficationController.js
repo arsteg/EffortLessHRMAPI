70872da0c50feb490e66f7041d003dc762df40b1
ARSTEG SOLUTIONS <info@arsteg.com>
2025-03-04 21:52:32 +0530

    added web socket code for logger

--- controllers/notficationController.js
+++ controllers/notficationController.js
@@ -12,8 +12,8 @@ const wss = new WebSocket.Server({ noServer: true });
 const connectedClients = new Set();
 
 // WebSocket upgrade listener for the HTTP server
-const server = app.listen(3000, () => {
-  console.log('WebSocket server is running on port 3000');
+const server = app.listen(4000, () => {
+  console.log('WebSocket server is running on port 4000');
 });
 
 server.on('upgrade', (request, socket, head) => {
