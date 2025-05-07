70e438609510f5114c13bbed5a611679a929f726
ARSTEG SOLUTIONS <info@arsteg.com>
2025-05-07 10:43:47 +0530

    updated field name

--- models/permissions/userModel.js
+++ models/permissions/userModel.js
@@ -284,7 +284,7 @@ userSchema.methods.createPasswordResetToken = function() {
 userSchema.pre(/^find/,async function(next) {
   this.populate({
     path: 'role',
-    select: 'id RoleName'
+    select: 'id name'
   })
   next();
 });
