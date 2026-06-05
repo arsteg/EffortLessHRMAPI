48d407540f6004cac03b765adfcee01a0bb5351a
Sandeep Kumar <sandeepkumar.chipsoft@gmail.com>
2026-06-05 20:49:55 +0530

    test api for ci cd

--- controllers/authController.js
+++ controllers/authController.js
@@ -1437,7 +1437,7 @@ async function seedRolePermissions(company) {
 exports.testApi = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: constants.APIResponseStatus.Success,
-    message: 'API called successfully 2',
+    message: 'API called successfully 3',
     data: {
       timestamp: new Date().toISOString(),
       endpoint: '/test'
